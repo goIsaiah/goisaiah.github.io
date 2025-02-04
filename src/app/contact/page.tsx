04590b3b67c1a7e78e417942ad7bea86db311dce
goIsaiah <goisaiah@outlook.com>
2025-02-03 19:49:30 -0500

    Made changes to contact

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -21,14 +21,33 @@ import Link from "next/link";
 import { Worker, Viewer } from '@react-pdf-viewer/core';
 import '@react-pdf-viewer/core/lib/styles/index.css';
 
+import { Github, Linkedin } from "lucide-react";
+
 export default function Home() {
     const router = useRouter();
     return (
         <main className="pt-16">
+            <div className="flex items-center justify-center h-[300px]">
+                <h1 className="text-5xl font-bold invert-text">Contact Me</h1>
+            </div>
+
+            <div className="flex flex-row items-center justify-center h-[300px] gap-4">
+                <Link href="https://github.com/goIsaiah">
+                    <Button className="px-6 py-3 text-lg flex items-center gap-2">
+                        <Github className="w-6 h-6" /> Github Account
+                    </Button>
+                </Link>
+
+                <Link href="https://www.linkedin.com/in/isaiah-gocool-7a0a08258/">
+                    <Button className="px-6 py-3 text-lg flex items-center gap-2 bg-[#0077b5] hover:bg-[#005a8c] text-white">
+                        <Linkedin className="w-6 h-6" /> Linkedin Profile
+                    </Button>
+                </Link>
+            </div>
             <div className="flex flex-col items-center justify-center">
                 <div className="flex justify-center items-center">
                     <iframe
-                        src="https://drive.google.com/file/d/1aWby5v-siGQ5I9q7iGCUmLdVtsgqD6x1/view?usp=sharing"
+                        src="https://drive.google.com/file/d/134oj0uXoascVAglRnsDgeZJk0c9K4uTe/view?usp=sharing"
                         className="w-[600px] h-[400px] border rounded-lg shadow-lg"
                     />
                 </div>
